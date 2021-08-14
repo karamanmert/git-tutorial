5bf5959ea2a58bd23ad42f5de3fcdf6581fcaa76
Mert Karaman <mertkrmn06@gmail.com>
2021-08-14 19:46:37 +0300

    NOTES ADDED

--- src/com/mertkaraman/Main.java
+++ src/com/mertkaraman/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Area area = new Area(new Square(5));
         System.out.println(area.calculateArea());
-           //mert
+
         Area area2 = new Area(new Rectangle(5,6));
         System.out.println(area2.calculateArea());
     }
